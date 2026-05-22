63baed9ed01dadfdf83a71eb605b2affbf0b10d3
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-05-22 20:08:28 +0500

    Fix image paths for github pages and use inline SVG for CV icon

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <div className="card">
             <div className="avatar-wrap">
                 <img
-                    src="/favicon.png"
+                    src="favicon.png"
                     alt="Abdul Rahman"
                     className="avatar-image"
                 />
@@ -73,7 +73,9 @@ function App() {
                     title="Download CV"
                     download
                 >
-                    <img src="/cv-svg.svg" alt="CV" style={{ width: '20px', height: '20px' }} />
+                    <svg viewBox="0 0 384 512" width="20" height="20" fill="currentColor">
+                        <path d="M64 464c-8.8 0-16-7.2-16-16V64c0-8.8 7.2-16 16-16H224v80c0 17.7 14.3 32 32 32h80V448c0 8.8-7.2 16-16 16H64zM64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V154.5c0-17-6.7-33.3-18.7-45.3L274.7 18.7C262.7 6.7 246.5 0 229.5 0H64zm56 256c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H264c13.3 0 24-10.7 24-24s-10.7-24-24-24H120z"/>
+                    </svg>
                 </a>
             </div>
             <div className="footer-note">
